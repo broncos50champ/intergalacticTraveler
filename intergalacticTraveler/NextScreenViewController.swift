f45b5ebe10072d5c7162e9eb900bd35973431fdd
Luis Segovia <luiissegovia@icloud.com>
2022-03-23 14:16:17 -0600

    Added new screen and image view

---
+++ intergalacticTraveler/NextScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextScreenViewController.swift
+//  intergalacticTraveler
+//
+//  Created by Luis Segovia on 3/23/22.
+//
+
+import UIKit
+
+class NextScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var ImageviewOutlet: UIImageView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
